30abf31a54ca0df7d5027134c8211e6b7c9b29d3
alen <alenbio42@gmail.com>
2020-06-10 10:15:08 +0700

    berhasil menggunakan props di react.js

--- src/Component/Fungsional/Home.jsx
+++ src/Component/Fungsional/Home.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 import App from '../../App'
+import Blog from './Blog'
 
 const Home = () => {
-    return <div><h1>ini adalah Home</h1></div>
+    return <div>
+        <Blog 
+        tanggal="02 Juni 2020"
+        judul="Teknologi Blockchain"
+        summary="Generate Lorem Ipsum placeholder text for use in your graphic"
+        />
+        <Blog 
+        tanggal="03 Juni 2020"
+        judul="Teknologi Internet of Things"
+        summary="Generate Lorem Ipsum placeholder text for use in your graphic"
+        />
+        <Blog 
+        tanggal="04 Juni 2020"
+        judul="Design Pattern Laravel"
+        summary="Generate Lorem Ipsum placeholder text for use in your graphic"
+        />
+    </div>
 }
 
 export default Home;
