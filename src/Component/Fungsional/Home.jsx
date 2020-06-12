a6f2182c7b3c3224cd3fcf3f35ca7b7170408120
alen <alenbio42@gmail.com>
2020-06-12 11:31:54 +0700

    berhasil menampilkan banyak produk berbeda

--- src/Component/Fungsional/Home.jsx
+++ src/Component/Fungsional/Home.jsx
@@ -5,7 +5,15 @@ import Produk from '../Class/Produk'
 
 const Home = () => {
     return <div>
-        <Produk nama="Mackbook Pro 2020" stock="10" harga="33000000"/></div>
+        
+        <Produk nama="Mackbook Pro 2020" stock="10" harga="33000000"/>
+        <Produk nama="Mackbook Pro 2019" stock="15" harga="53000000"/>
+        <Produk nama="Mackbook Pro 2021" stock="14" harga="63000000"/>
+        <Produk nama="Mackbook Pro 2022" stock="50" harga="37000000"/>
+        <Produk nama="Mackbook Pro 2023" stock="100" harga="39000000"/>
+        <Produk nama="Mackbook Pro 2024" stock="108" harga="342000000"/>
+        
+        </div>
         //<Blog/ 
         //tanggal="02 Juni 2020"
         //judul="Teknologi Blockchain"
@@ -20,6 +28,7 @@ const Home = () => {
         //tanggal="04 Juni 2020"
         //judul="Design Pattern Laravel"
         //summary="Generate Lorem Ipsum placeholder text for use in your graphic"
+        
 }
 
 export default Home;
