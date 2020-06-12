6680c112cac0d22607c99be98b1b87bd24c762be
alen <alenbio42@gmail.com>
2020-06-12 11:16:57 +0700

    berhasil menampilkan stock produk

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import Home from './Component/Fungsional/Home';
 //import Beranda from './Component/Class/Beranda';
 //import './App.css';
--- src/Component/Fungsional/Home.jsx
+++ src/Component/Fungsional/Home.jsx
@@ -1,27 +1,25 @@
 import React from 'react'
-import App from '../../App'
+//import App from '../../App'
 import Produk from '../Class/Produk'
 //import Blog from './Blog'
 
 const Home = () => {
     return <div>
-        { <Produk/>
-        /*<Blog 
-        tanggal="02 Juni 2020"
-        judul="Teknologi Blockchain"
-        summary="Generate Lorem Ipsum placeholder text for use in your graphic"
-        />
-        <Blog 
-        tanggal="03 Juni 2020"
-        judul="Teknologi Internet of Things"
-        summary="Generate Lorem Ipsum placeholder text for use in your graphic"
-        />
-        <Blog 
-        tanggal="04 Juni 2020"
-        judul="Design Pattern Laravel"
-        summary="Generate Lorem Ipsum placeholder text for use in your graphic"
-        />*/}
-    </div>
+        <Produk nama="Mackbook Pro 2020" stock="10" harga="33000000"/></div>
+        //<Blog/ 
+        //tanggal="02 Juni 2020"
+        //judul="Teknologi Blockchain"
+        //summary="Generate Lorem Ipsum placeholder text for use in your graphic"
+        //>
+        //<Blog/ 
+        //tanggal="03 Juni 2020"
+        //judul="Teknologi Internet of Things"
+        //summary="Generate Lorem Ipsum placeholder text for use in your graphic"
+        ///>
+        //<Blog/
+        //tanggal="04 Juni 2020"
+        //judul="Design Pattern Laravel"
+        //summary="Generate Lorem Ipsum placeholder text for use in your graphic"
 }
 
 export default Home;
