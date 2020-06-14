f50511e8f10a7e04a3c0fb1e1eb8e3e32796294f
alen <alenbio42@gmail.com>
2020-06-14 11:59:42 +0700

    berhasil menampilkan Hello Bootstrap

---
+++ src/Component/Class/BootstrapComp.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class BootstrapComp extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            
+        }
+    }
+
+    render() {
+    return (
+        <div>Hello Bootstrap</div>
+        )
+    }
+}
+
+export default BootstrapComp;
